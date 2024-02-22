764349b50c062573eb69b306e16d566bff715469
Muasa-harman <harman.muasa@gmail.com>
2024-02-22 11:31:07 +0300

    create restaurant api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,13 +2,21 @@ import express,{Request,Response} from 'express';
 import cors from "cors";
 import "dotenv/config";
 import mongoose from 'mongoose';
-import myUserRoute from "./routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import path from 'path';
+import { v2 as cloudinary } from "cloudinary";
 
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=>{
     console.log("db coonected successfully")
 });
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+console.log("Cloudinary configured successfully.");
 
 
 const app = express();
@@ -21,9 +29,10 @@ app.use(express.static(path.join(__dirname,"../../frontend/dist")));
 
 // /api/my/user
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute)
 
-app.get("/test", async (req:Request, res:Response) =>{
-    res.json({message: "Hello!"});
+app.get("/health", async (req:Request, res:Response) =>{
+    res.send({message: "OK!"});
 });
 
 app.get("*", (req:Request,res:Response)=>{
---
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -0,0 +1,9 @@
+type Props = {
+   onSave: (restaurantFormData: FormData) => void;
+};
+
+const ManageRestaurantForm = ({}: Props) => {
+
+}
+
+export default ManageRestaurantForm;
