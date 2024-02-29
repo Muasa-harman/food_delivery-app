7b046c0b59d9257852c627bfbdb976304fa7d36b
Muasa-harman <harman.muasa@gmail.com>
2024-02-29 13:48:48 +0300

    filter component

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,11 +1,92 @@
-import { useParams } from "react-router-dom"
+import { useSearchRestaurants } from "@/api/RestaurantApi";
+import CuisineFilter from "@/components/CuisineFilter";
+import PaginationSelector from "@/components/PaginationSelector";
+import SearchBar, { SearchForm } from "@/components/SearchBar";
+import SearchResultInfo from "@/components/SearchResultInfo";
+import SearchResultsCard from "@/components/SearchResultsCard";
+import { useState } from "react";
+import { useParams } from "react-router-dom";
 
+export type SearchState = {
+  searchQuery: string;
+  page: number;
+  selectedCuisines: string[];
+};
 
 const SearchPage = () => {
-    const {city} = useParams();
-  return (
-    <span>User searched for {city}</span>
-  )
-}
+  const { city } = useParams();
+  const [SearchState, setSearchState] = useState<SearchState>({
+    searchQuery: "",
+    page: 1,
+    selectedCuisines: [],
+  });
+  const { results, isLoading } = useSearchRestaurants(SearchState, city);
 
-export default SearchPage
+  const setSelectedCuisines = (selectedCuisines: string[])=>{
+    setSearchState((prevState)=>({
+      ...prevState,
+      selectedCuisines,
+      page:1
+    }))
+  }
+
+  const setPage = (page: number) =>{
+    setSearchState((prevState)=>({
+      ...prevState,
+      page,
+    }));
+  };
+
+  const setSearchQuery = (searchFormData: SearchForm) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFormData.searchQuery,
+      page: 1,
+    }));
+  };
+
+  const resetSearch = () => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: "",
+      page: 1,
+    }));
+  };
+
+  if (isLoading) {
+    <span>Loading...</span>;
+  }
+
+  if (!results?.data || !city) {
+    return <span>No results found</span>;
+  }
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div className="" id="cuisines-list">
+        <CuisineFilter selectedCuisines={SearchState.selectedCuisines}
+        onChange={setSelectedCuisines}
+        />
+      </div>
+      <div className="flex flex-col gap-5" id="main-content">
+        <SearchBar
+          searchQuery={SearchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeHolder="Search by Cuisine or Restaurant Name"
+          onReset={resetSearch}
+        />
+        <SearchResultInfo total={results.pagination.total} city={city} />
+        {results.data.map((restaurant) => (
+          <SearchResultsCard restaurant={restaurant} />
+        ))}
+        <PaginationSelector
+          page={results.pagination.page}
+          pages={results.pagination.pages}
+          onPageChange={setPage}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SearchPage;
